5d9749e4b1d143296a2ad1585f15eac368e4ea82
phb98 <phanhaibac98@gmail.com>
2023-05-07 10:13:37 +0700

    Update GPSense_conf to config console print level

--- GPSense_conf.h
+++ GPSense_conf.h
@@ -12,12 +12,40 @@
 
 
 // PRINTF MACRO HERE
-#define GPS_PRINTF printf
+#define GPS_DEBUG_LEVEL_INFO    1
+#define GPS_DEBUG_LEVEL_ERROR   2
+#define GPS_DEBUG_LEVEL_DEBUG   3
+#define GPS_DEBUG_LEVEL_VERBOSE 4
+#define GPS_PRINTF(...)         printf(__VA_ARGS__)
 
-#define GPS_LOGI(fmt, ...) GPS_PRINTF("GPS\t[I]\t%s(): " fmt " \r\n",__func__, ##__VA_ARGS__)
-#define GPS_LOGE(fmt, ...) GPS_PRINTF("GPS\t[E]\t%s(): " fmt " \r\n",__func__, ##__VA_ARGS__)
-#define GPS_LOGD(fmt, ...) GPS_PRINTF("GPS\t[D]\t%s(): " fmt " \r\n",__func__, ##__VA_ARGS__)
-#define GPS_LOGV(fmt, ...) GPS_PRINTF("GPS\t[V]\t%s(): " fmt " \r\n",__func__, ##__VA_ARGS__)
+#ifndef DEBUG_LEVEL
+  #define DEBUG_LEVEL GPS_DEBUG_LEVEL_INFO
+#endif
+
+#if DEBUG_LEVEL >= GPS_DEBUG_LEVEL_INFO
+  #define GPS_LOGI(fmt, ...) GPS_PRINTF("GPS\t[I]\t%s(): " fmt " \r\n",__func__, ##__VA_ARGS__)
+#else
+  #define GPS_LOGI(format, ...)
+#endif
+
+#if DEBUG_LEVEL >= GPS_DEBUG_LEVEL_ERROR
+  #define GPS_LOGE(fmt, ...) GPS_PRINTF("GPS\t[E]\t%s(): " fmt " \r\n",__func__, ##__VA_ARGS__)
+#else
+  #define GPS_LOGE(format, ...)
+#endif
+
+#if DEBUG_LEVEL >= GPS_DEBUG_LEVEL_DEBUG
+  #define GPS_LOGD(fmt, ...) GPS_PRINTF("GPS\t[D]\t%s(): " fmt " \r\n",__func__, ##__VA_ARGS__)
+#else
+  #define GPS_LOGD(format, ...)
+#endif
+
+#if DEBUG_LEVEL >= GPS_DEBUG_LEVEL_VERBOSE
+  #define GPS_LOGV(fmt, ...) GPS_PRINTF("GPS\t[V]\t%s(): " fmt " \r\n",__func__, ##__VA_ARGS__)
+#else
+  #define GPS_LOGV(format, ...)
+#endif
+
 
 
 /************************************************************************************************************/
